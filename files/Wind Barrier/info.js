aaba0ed463cca54b88882f505d0fc22fce1401a4
ShadowVr122 <jallaladam9@gmail.com>
2025-05-05 21:23:26 -0400

    Create info.js

---
+++ files/Wind Barrier/info.js
@@ -0,0 +1,7 @@
+{
+Name: Wind Barrier;
+Description: A wind barrier so players dont get out of the map;
+Requirements: Visual Scripting (Unity Registery);
+Creators: Jumpman Dev;
+VersionCode: 1;
+}
